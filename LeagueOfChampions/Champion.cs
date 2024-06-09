c52c8cf1b34af578ed71ba2ae3e2714f4a058a1f
DGojka <dawidgojka4@gmail.com>
2024-06-09 21:50:49 +0200

    initial commit

---
+++ LeagueOfChampions/Champion.cs
@@ -0,0 +1,5 @@
+﻿namespace LeagueOfChampions; 
+
+public class Champion {
+    
+}
---
+++ LeagueOfChampions/MainClass.cs
@@ -0,0 +1,7 @@
+﻿namespace LeagueOfChampions; 
+
+public class MainClass {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello, Worlddd!");
+    }
+}
