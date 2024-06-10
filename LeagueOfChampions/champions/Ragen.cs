0eea560d05af9a8964b48a00fe0fae0262b9f29b
DGojka <dawidgojka4@gmail.com>
2024-06-10 23:18:55 +0200

    New Champion: Ragen

---
+++ LeagueOfChampions/champions/Ragen.cs
@@ -0,0 +1,99 @@
+﻿using LeagueOfChampions.champions.@base;
+using LeagueOfChampions.spell;
+
+namespace LeagueOfChampions.champions;
+
+public class Ragen : Champion {
+    private const string RAGEN = "Ragen";
+    private SubtitlesPrinter _subtitlesPrinter;
+
+    //stats
+    private const int MAX_HP = 400;
+    private const int ATTACK_DAMAGE = 20;
+    private const int ARMOR = 40;
+    private const int MAX_MANA = 4;
+
+    //mana costs
+    private const int BasicAttackManaCost = 1;
+    private const int QManaCost = 2;
+    private const int WManaCost = 1;
+    private const int EManaCost = 2;
+    private const int RManaCost = MAX_MANA;
+
+    //spells
+    //Q
+    private const int QDamage = ATTACK_DAMAGE * 3;
+
+    //W
+    private const int WArmorGain = 2;
+
+    //E
+    private const int SingleSpinDamage = 7;
+    private const int MinSpinsCount = 0;
+
+    private const int MaxSpinsCount = 13;
+
+    //R
+    private const int RDamage = 50;
+
+    //PASSIVE
+    private const int MinHpToUsePassive = MAX_HP - 30;
+
+    public Ragen(SubtitlesPrinter subtitlesPrinter) : base(subtitlesPrinter, RAGEN) {
+        MaxHp = MAX_HP;
+        CurrentHp = MAX_HP;
+        Armor = ARMOR;
+        AttackDamage = ATTACK_DAMAGE;
+        ManaPoints = MAX_MANA;
+        CurrentManaPoints = ManaPoints;
+        _subtitlesPrinter = subtitlesPrinter;
+    }
+
+    public override Spell? ProvideBasicAttack() {
+        return BasicAttack ??=
+            new Spell(
+                new SpellDescription(AttackDamage, false, false), BasicAttackManaCost, () => { });
+    }
+
+    public override Spell ProvideQ() {
+        return SpellQ ??=
+            new Spell(
+                new SpellDescription(QDamage, false, false), QManaCost, () => { });
+    }
+
+    public override Spell ProvideW() {
+        return SpellW ??=
+            new Spell(new SpellDescription(), WManaCost, () => { Armor += WArmorGain; });
+    }
+
+    public override Spell ProvideE() {
+        var spinsDamage = 0;
+        var spinsCount = MathHelper.RandomInt(MinSpinsCount, MaxSpinsCount);
+        _subtitlesPrinter.RagenPrintSpinsCount(spinsCount);
+        for (var i = MinSpinsCount; i < spinsCount; i++) {
+            spinsDamage += SingleSpinDamage;
+        }
+
+        SpellE = new Spell(new SpellDescription(spinsDamage, false, false), EManaCost, () => { });
+
+        return SpellE;
+    }
+
+    public override Spell ProvideR() {
+        return SpellR ??=
+            new Spell(new SpellDescription(RDamage, true, true), RManaCost, () => { });
+    }
+
+    public override Spell ProvidePassive() {
+        return PassiveSpell ??= new Spell(new SpellDescription(),
+            0, () => {
+                if (CurrentHp <= MinHpToUsePassive) {
+                    int regeneratedHp = (int)((MaxHp - CurrentHp) * 0.035);
+                    CurrentHp += regeneratedHp;
+                    _subtitlesPrinter.RagenPrintPassive(regeneratedHp);
+                }
+
+                Armor++;
+            });
+    }
+}
--- LeagueOfChampions/helpers/SubtitlesPrinter.cs
+++ LeagueOfChampions/helpers/SubtitlesPrinter.cs
@@ -1,78 +1,77 @@
 ﻿using LeagueOfChampions.champions.@base;
 
-namespace LeagueOfChampions; 
+namespace LeagueOfChampions;
+
 using System;
 
-public class SubtitlesPrinter
-{
-    public void PrintEnter(int enterCount)
-    {
-        for (int i = 0; i < enterCount; i++)
-        {
+public class SubtitlesPrinter {
+    public void PrintEnter(int enterCount) {
+        for (int i = 0; i < enterCount; i++) {
             Println(" ");
         }
     }
 
     //GAME
-    public void PrintRoundCount(int roundCount)
-    {
+    public void PrintRoundCount(int roundCount) {
         Println("ROUND: " + roundCount + ". FIGHT!");
     }
 
-    public void PrintTurn(string championName)
-    {
+    public void PrintTurn(string championName) {
         Println("================== " + championName + "'s turn! ==================");
     }
 
-    public void PrintHp(Champion champion)
-    {
+    public void PrintHp(Champion champion) {
         Println(champion.GetType().Name + ": " + champion.GetHp() + " hp");
     }
 
-    public void PrintWinner(string championName)
-    {
+    public void PrintWinner(string championName) {
         Println(championName + " has won!");
     }
 
-    public void PrintActionPoints(int actionPoints)
-    {
+    public void PrintActionPoints(int actionPoints) {
         Println("Remaining action points: " + actionPoints);
     }
 
-    public void PrintChampionList(List<Champion> champions)
-    {
+    public void PrintChampionList(List<Champion> champions) {
         Println("List of the Champions: " + string.Join(", ", champions));
     }
 
-    public void PrintPlayerToPickNumber(int playerNumber)
-    {
+    public void PrintPlayerToPickNumber(int playerNumber) {
         Print("Player " + playerNumber + ": ");
     }
 
-    public void PrintStartBattle()
-    {
+    public void PrintStartBattle() {
         Println("The battle will begin in 2 seconds!");
     }
 
-    public void PrintChosenChampion(string championName)
-    {
+    public void PrintChosenChampion(string championName) {
         Console.WriteLine("You've chosen " + championName);
     }
 
     //CHAMPION SECTION
-    public void SpellOnCooldown()
-    {
+    public void SpellOnCooldown() {
         Println("Your spell is on cooldown! Wait for the next round.");
     }
 
+    public void DamageTaken(string victimName, int damageTaken) {
+        Println(victimName + "has suffered " + damageTaken + ". ");
+    }
+
+    //GAREN
+    public void RagenPrintPassive(int regeneratedHp) {
+        Println("Perseverance: Garen regenerated " + regeneratedHp + "HP.");
+    }
+
+    public void RagenPrintSpinsCount(int spins) {
+        Println("Ragen spun " + spins + " times.");
+    }
+
     //OTHERS
-    private void Print(string text)
-    {
+    private void Print(string text) {
         Console.Write(text);
     }
 
-    private void Println(string text)
-    {
+    private void Println(string text) {
         Console.WriteLine(text);
     }
 }
