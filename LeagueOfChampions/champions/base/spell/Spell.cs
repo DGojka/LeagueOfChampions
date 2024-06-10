28952abb8950e117d1dda8a62909dc9b982068d7
DGojka <dawidgojka4@gmail.com>
2024-06-10 19:51:26 +0200

    Spell

---
+++ LeagueOfChampions/champions/base/spell/Spell.cs
@@ -0,0 +1,23 @@
+﻿namespace LeagueOfChampions.spell;
+
+public class Spell {
+    public SpellDescription SpellDescription { get; set; }
+    public int ManaPointsCost { get; set; }
+    public bool IsSpellOnCooldown { get; set; }
+    private readonly Action _spellListener;
+
+    public Spell(SpellDescription spellDescription, int manaPointsCost, Action spellListener) {
+        SpellDescription = spellDescription;
+        ManaPointsCost = manaPointsCost;
+        IsSpellOnCooldown = false;
+        _spellListener = spellListener;
+    }
+
+    public void Use() {
+        _spellListener();
+    }
+}
+
+public interface ISpellListener {
+    void OnSpellUsed();
+}
---
+++ LeagueOfChampions/champions/base/spell/SpellDescription.cs
@@ -0,0 +1,23 @@
+﻿namespace LeagueOfChampions.spell;
+
+public class SpellDescription {
+    public int AttackDamage { get; set; }
+    public bool TrueDmg { get; set; }
+    public float ArmorPen { get; set; }
+    public bool DamageByMissingHp { get; set; }
+
+    public SpellDescription(int attackDamage, int apDmg, bool trueDmg, float armorPen,
+        float magicPen, bool damageByMissingHp) {
+        AttackDamage = attackDamage;
+        TrueDmg = trueDmg;
+        ArmorPen = armorPen;
+        DamageByMissingHp = damageByMissingHp;
+    }
+
+    public SpellDescription() {
+        AttackDamage = 0;
+        TrueDmg = false;
+        ArmorPen = 0;
+        DamageByMissingHp = false;
+    }
+}
