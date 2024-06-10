ecbe1284afd38cb1622ad35fbf15e9de6e66e4f8
DGojka <dawidgojka4@gmail.com>
2024-06-10 23:18:06 +0200

    Game

---
+++ LeagueOfChampions/Game.cs
@@ -0,0 +1,96 @@
+﻿using LeagueOfChampions.champions.@base;
+using LeagueOfChampions.spell;
+
+namespace LeagueOfChampions;
+
+public class Game {
+    private readonly Champion champion1;
+    private readonly Champion champion2;
+    private int _roundCount;
+    private readonly SubtitlesPrinter subtitlesPrinter;
+
+    public Game(Champion champion1, Champion champion2, SubtitlesPrinter subtitlesPrinter) {
+        this.subtitlesPrinter = subtitlesPrinter;
+        this.champion1 = champion1;
+        this.champion2 = champion2;
+    }
+
+    public void Start() {
+        Champion championInMove = champion1;
+        Champion attackedChampion = champion2;
+
+        int turnCount = 0;
+        while (!SomeChampionDied()) {
+            turnCount++;
+            if (turnCount % 2 != 0) {
+                _roundCount++;
+                Console.Clear();
+                
+                ResetAbilities(championInMove, attackedChampion);
+                UsePassiveSpells(championInMove, attackedChampion);
+                PrintOnBeginningOfTheRound();
+            }
+
+            while (championInMove.GetCurrentManaPoints() > 0 && attackedChampion.GetHp() > 0) {
+                subtitlesPrinter.PrintActionPoints(championInMove.GetCurrentManaPoints());
+                Spell? spell;
+                do {
+                    spell = GetSpell(KeyboardManager.GetKey(), championInMove);
+                } while (spell == null);
+
+                spell.Use();
+                attackedChampion.ReceiveSpell(spell.SpellDescription);
+                subtitlesPrinter.PrintEnter(1);
+                subtitlesPrinter.PrintHp(champion2);
+                subtitlesPrinter.PrintHp(champion1);
+            }
+            if (SomeChampionDied()) {
+                Champion winner = DetermineWinner();
+                subtitlesPrinter.PrintWinner(winner.GetName());
+            } else {
+                (championInMove, attackedChampion) = (attackedChampion, championInMove);
+                subtitlesPrinter.PrintTurn(championInMove.GetName());
+            }
+        }
+    }
+
+    private Spell? GetSpell(KeyType type, Champion champion) {
+        return type switch {
+            KeyType.T => champion.BaseAttackHandler(),
+            KeyType.Q => champion.SpellQHandler(),
+            KeyType.W => champion.SpellWHandler(),
+            KeyType.E => champion.SpellEHandler(),
+            KeyType.R => champion.SpellRHandler(),
+            _ => null,
+        };
+    }
+
+    private bool SomeChampionDied() {
+        return champion1.GetHp() <= 0 || champion2.GetHp() <= 0;
+    }
+
+    private Champion DetermineWinner() {
+        return champion1.GetHp() < champion2.GetHp() ? champion2 : champion1;
+    }
+    
+    private void ResetAbilities(Champion championInMove, Champion attackedChampion) {
+        championInMove.ResetCurrentManaPoints();
+        attackedChampion.ResetCurrentManaPoints();
+        championInMove.ResetCooldowns();
+        attackedChampion.ResetCooldowns();
+    }
+
+    private void UsePassiveSpells(Champion championInMove, Champion attackedChampion) {
+        championInMove.ProvidePassive().Use();
+        attackedChampion.ProvidePassive().Use();
+    }
+
+    private void PrintOnBeginningOfTheRound() {
+        subtitlesPrinter.PrintHp(champion1);
+        subtitlesPrinter.PrintHp(champion2);
+        subtitlesPrinter.PrintEnter(1);
+        subtitlesPrinter.PrintRoundCount(_roundCount);
+        subtitlesPrinter.PrintEnter(1);
+        subtitlesPrinter.PrintTurn(champion1.GetName());
+    }
+}
--- LeagueOfChampions/MainClass.cs
+++ LeagueOfChampions/MainClass.cs
@@ -1,7 +1,25 @@
-﻿namespace LeagueOfChampions; 
+﻿using LeagueOfChampions.managers;
+
+namespace LeagueOfChampions;
 
 public class MainClass {
-    public static void Main(string[] args) {
-        Console.WriteLine("Hello, Worlddd!");
+    public static void Main() {
+        var subtitlesPrinter = new SubtitlesPrinter();
+        var champions = ChampionCreator.CreateChampions(subtitlesPrinter);
+        var pickingHandler = new ChampionPicker(champions);
+
+        // Picking Champions
+        subtitlesPrinter.PrintChampionList(champions);
+        subtitlesPrinter.PrintPlayerToPickNumber(1);
+        var champion1 = pickingHandler.PickChampion();
+
+        subtitlesPrinter.PrintChampionList(champions);
+        subtitlesPrinter.PrintPlayerToPickNumber(2);
+        var champion2 = pickingHandler.PickChampion();
+
+        subtitlesPrinter.PrintStartBattle();
+
+        var game = new Game(champion1, champion2, subtitlesPrinter);
+        game.Start();
     }
 }
