a39b885f3ca6ee5b653661c937794ad7e8dc14df
DGojka <dawidgojka4@gmail.com>
2024-06-22 12:43:20 +0200

    rayz spell explanation

--- LeagueOfChampions/champions/RayZ/RayZSpellsExplanation.cs
+++ LeagueOfChampions/champions/RayZ/RayZSpellsExplanation.cs
@@ -1,21 +1,27 @@
 ﻿using LeagueOfChampions.spell;
 
-namespace LeagueOfChampions.champions; 
+namespace LeagueOfChampions.champions;
 
 public class RayZSpellsExplanation : SpellsExplanation {
     public string GetQExplanation() {
-        throw new NotImplementedException();
+        return
+            $"Q deals doubled damage. If enemy is marked it deals 4x damage. You can mark enemy with E. {ManaCost(RayZConstants.QManaCost)}";
     }
 
     public string GetWExplanation() {
-        throw new NotImplementedException();
+        return $"W has 6 random usages, you can either gain mana point or mark enemy.{ManaCost(RayZConstants.WManaCost)}";
     }
 
     public string GetEExplanation() {
-        throw new NotImplementedException();
+        return $"Deals some small damage and marks enemy. {ManaCost(RayZConstants.EManaCost)}";
     }
 
     public string GetRExplanation() {
-        throw new NotImplementedException();
+        return
+            "Consumes current mana and resets every cooldown and mana points to its initial state.";
+    }
+
+    private string ManaCost(int manaCost) {
+        return $"Mana cost: {manaCost}";
     }
 }
